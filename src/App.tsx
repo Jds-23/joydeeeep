2f607443545d53f62a8395a96ecb686c2d60ce41
Joydeep <joydeepsingha68@gmail.com>
2025-06-10 21:01:09 +0530

    revived ✨

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <>
+      <h2 className="text-xl font-bold ">Hello there, this page is made by Joydeep.</h2>
+      <p className="text-sm">Who's that?</p>
+      <p className="text-sm">A male born in 2001 in Purulia, WB, India.</p>
+      <p className="text-sm">Currently I code for a living. And i like it too. Anyways, that's only thing I'm decent in.</p>
+      <p className="text-sm">I also used to enjoyed MCU movies(all before Endgame and Loki series).</p>
+      <p className="text-sm">A fan of <a href="https://www.kkr.in/" className="text-blue-500">KKR</a> and <a href="https://www.fcbarcelona.com/" className="text-blue-500">FC Barcelona</a>. I believe Messi's the 🐐</p>
+      <p className="text-sm">Recently I've been watching animes, Demon Slayer is my favorite. Currently watching One Piece.(could be my new favorite)</p>
+      <p className="text-sm">Talk to me about ethereum UX(aa,interop,wallets,etc) <a href="https://twitter.com/0xJoydeeeep" className="text-blue-500">Twitter</a>. Check out my <a href="https://github.com/Jds-23" className="text-blue-500">Github</a>. I also write sometimes on <a href="https://substack.com/@0xjoydeeeep" className="text-blue-500">substack</a>.</p>
+    </>
+  )
+}
+
+export default App
